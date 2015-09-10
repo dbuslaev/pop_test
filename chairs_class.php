ad39df9b37010b0182239a6b1d90939a0da0dc29
Dmitry Buslaev <info@buslaev.com>
2015-09-09 18:39:49 -0700

    added class

---
+++ chairs_class.php
@@ -0,0 +1,70 @@
+<?php
+/*
+Take a second to imagine that you are in a room with 100 chairs arranged in a circle. These chairs are numbered sequentially from One to One Hundred.
+
+At some point in time, the person in chair #1 will be told to leave the room. The person in chair #2 will be skipped, and the person in chair #3 will be told to leave. Next to go is person in chair #6. In other words, 1 person will be skipped initially, and then 2, 3, 4.. and so on. This pattern of skipping will keep going around the circle until there is only one person remaining.. the survivor. Note that the chair is removed when the person leaves the room.
+*/
+
+class ChairsCircle {
+
+  var $chairs_total;
+  var $chairs_skip_increment;
+  var $chairs_sequence;
+
+  function ChairsCircle($chairs_total = 100, $chairs_skip_increment = 1){
+    $this->chairs_total = $chairs_total;
+    $this->chairs_skip_increment = $chairs_skip_increment;
+  }
+
+  function getSurvivingChairNumber(){
+    $chairs_sequence = $this->getSurvivingSequence();
+    return array_pop($chairs_sequence);
+  }
+
+  // creates sequence only the first time and saves it for later
+  function getSurvivingSequence(){
+    if(!is_array($this->chairs_sequence)){
+      $this->chairs_sequence = $this->createSurvivingSequence();
+    }
+    return $this->chairs_sequence;
+  }
+
+  private function createSurvivingSequence(){
+    $chairs_total = $this->chairs_total;
+    $chairs_skip_increment = $this->chairs_skip_increment;
+    $chairs_circle = range(1, $chairs_total);
+    $chairs_current = 0;
+    $chairs_to_skip = 0;
+    $chairs_sequence = [];
+    
+    // the last chair removed will be the number we want
+    while($chairs_total > 0){
+      // find the next person to leave the room
+      $chairs_current += $chairs_to_skip;
+      if($chairs_current >= $chairs_total) $chairs_current = $chairs_current % $chairs_total;
+    
+      // track removed chairs
+      $chairs_sequence[] = $chairs_circle[$chairs_current];
+      
+      // chair is removed
+      unset($chairs_circle[$chairs_current]);
+      $chairs_circle = array_values($chairs_circle);
+      $chairs_total --;
+    
+      // next time skip more chairs
+      $chairs_to_skip += $chairs_skip_increment;
+    }
+    return $chairs_sequence;
+  }
+}
+
+$circle = new ChairsCircle(100, 1);
+$survivor = $circle->getSurvivingChairNumber();
+$chairs_sequence = $circle->getSurvivingSequence();
+
+echo "The Survivor is: " . $survivor . PHP_EOL . PHP_EOL;
+
+echo "The Order of who left the room is:" . PHP_EOL;
+foreach($chairs_sequence as $key => $chair_number){
+  echo "#" . ($key + 1) . ": ".$chair_number . PHP_EOL;
+}
